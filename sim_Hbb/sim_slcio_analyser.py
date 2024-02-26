6a4d2bb2b198bd5cd135c72b808f472d2ade73f0
Lrozzy <lrozanov@uchicago.edu>
2024-02-26 16:06:14 -0600

    Cleaned up sim file analyser

--- sim_Hbb/sim_slcio_analyser.py
+++ sim_Hbb/sim_slcio_analyser.py
@@ -15,19 +15,15 @@ ROOT.gROOT.SetBatch()
 max_events = -1
 
 # Gather input files
-fnames = glob.glob("/local/d1/lrozanov/mucoll-tutorial-2023/sim_Hbb/stau_output_sim.slcio") 
+fnames = glob.glob("/local/d1/lrozanov/mucoll-tutorial-2023/digi_Hbb_bib/stau_output_digi_bib_10.slcio") 
 print("Found %i files."%len(fnames))
 
 # Create empty lists for each variable
-mcp_pt = [] #mcp = MCParticle (truth)
+mcp_pt = [] #mcp = monte-carlo particle (truth)
 mcp_phi = []
 mcp_eta = []
 pdgid = []
 status = []
-
-mcp_mu_pt = [] #mcp_mu = MCParticle muon
-mcp_mu_phi = []
-mcp_mu_eta = []
 
 i = 0
 # reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
@@ -38,15 +34,10 @@ for f in fnames:
     if f == "/data/fmeloni/DataMuC_MuColl10_v0A/reco/merged/muonGun_pT_1000_5000.slcio":
         continue
     if max_events > 0 and i >= max_events: break
-    #if no_inner_hits > np.abs(max_events): break
-    # if f != "/data/fmeloni/DataMuC_MuColl10_v0A/reco/merged/muonGun_pT_250_1000.slcio":
-    #     print("Skipped")
-    #     continue
     reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
     reader.open(f)
     for event in reader: 
         if max_events > 0 and i >= max_events: break
-        #if no_inner_hits > np.abs(max_events): break
         if i%1 == 0: print("Processing event %i."%i)
 
         mcpCollection = event.getCollection("MCParticle")
@@ -74,18 +65,6 @@ for f in fnames:
             ipdgid.append(pdg)
             istatus.append(mcp.getGeneratorStatus())
             # print("PID, Status, pT, eta, phi: ", pdg, mcp.getGeneratorStatus(), mcp_tlv.Perp(), mcp_tlv.Eta(), mcp_tlv.Phi())
-
-            if abs(mcp.getPDG())==13 and mcp.getGeneratorStatus()==1:
-
-                imcp_mu_pt = []
-                imcp_mu_eta = []
-                imcp_mu_phi = []
-
-                imcp_mu_pt.append(mcp_tlv.Perp())
-                imcp_mu_eta.append(mcp_tlv.Eta())
-                imcp_mu_phi.append(mcp_tlv.Phi())
-                n_mcp_mu += 1
-                # print("Truth pt, eta, phi:", mcp_tlv.Perp(), mcp_tlv.Eta(), mcp_tlv.Phi())
                         
         # print("End of event \n")
         # This is here to check that we never reconstruct multiple muons
@@ -97,10 +76,6 @@ for f in fnames:
         mcp_phi.append(imcp_phi)
         pdgid.append(ipdgid)
         status.append(istatus)
-        if n_mcp_mu > 0:
-            mcp_mu_pt.append(imcp_mu_pt)
-            mcp_mu_eta.append(imcp_mu_eta)
-            mcp_mu_phi.append(imcp_mu_phi)
        
     reader.close()
 
@@ -108,8 +83,6 @@ for f in fnames:
 print("\nSummary statistics:")
 print("Ran over %i events."%i)
 print("Found:")
-#print("\t%i MCPs"%hists["mcp_pt"].GetEntries())
-#print("\t%i mu MCPs"%hists["mcp_mu_pt"].GetEntries())
 print("\tSanity check mcpCollection length:", len(imcp_pt))
 
 # Make a list of all the data you want to save
@@ -119,12 +92,9 @@ data_list["mcp_eta"] = mcp_eta
 data_list["mcp_phi"] = mcp_phi
 data_list["pdgid"] = pdgid
 data_list["status"] = status
-data_list["mcp_mu_pt"] = mcp_mu_pt
-data_list["mcp_mu_eta"] = mcp_mu_eta
-data_list["mcp_mu_phi"] = mcp_mu_phi
 
 # After the loop is finished, save the data_list to a .json file
-output_json = "sim_Hbb/stau_output_sim.json"
+output_json = "stau_output_digi_bib_10.json"
 with open(output_json, 'w') as fp:
     json.dump(data_list, fp)
 
